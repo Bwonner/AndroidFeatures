41b1945603333337d6875270442ce2ee368a03c5
Svetlana Maslak <svetlana.mslk29@gmail.com>
2023-04-19 17:46:41 +0400

    Fix detekt errors

--- core/navigation/src/main/java/com/example/navigation/NavExtensions.kt
+++ core/navigation/src/main/java/com/example/navigation/NavExtensions.kt
@@ -27,6 +27,6 @@ fun NavController.deepLinkNavigateTo(
 }
 
 sealed class DeepLinkDestination(val address: String) {
-    class Dashboard(msg: String) : DeepLinkDestination("example://dashboard/exampleArgs?msg=${msg}")
+    class Dashboard(msg: String) : DeepLinkDestination("example://dashboard/exampleArgs?msg=$msg")
     object Next : DeepLinkDestination("example://next")
 }
